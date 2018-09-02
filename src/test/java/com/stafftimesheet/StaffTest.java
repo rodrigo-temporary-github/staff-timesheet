1b2e5d03bf2e3b5cb1e21815fc5addb31ba14d74
Rodrigo Bonilha Tolledo <rodrigotolledo@gmail.com>
2018-09-02 19:32:04 +1000

    replacing TestNG by JUnit as it integrates better with gradle

--- src/test/java/com/stafftimesheet/StaffTest.java
+++ src/test/java/com/stafftimesheet/StaffTest.java
@@ -1,7 +1,7 @@
 package com.stafftimesheet;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StaffTest {
 
